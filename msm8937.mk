9f570ae0094dbbcdb64412fcee9afe8927c616f7
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:18:33 +0200

    msm8937-common: Build USB HIDL HAL service
    Change-Id: I993d76c70f50ae6f8f6c3f468d9a363fad6711d5

--- msm8937.mk
+++ msm8937.mk
@@ -265,6 +265,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level-0.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version-1_0_3.xml
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl
