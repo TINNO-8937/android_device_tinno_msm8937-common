a314b347ab5d95f8618aa313493d530349654b04
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:07:43 +0200

    msm8937-common: Build Audio HIDL HAL interfaces
    Change-Id: Ide624d3008739b68a1b4cac347237e72cd4b4b20

--- msm8937.mk
+++ msm8937.mk
@@ -83,6 +83,10 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audiod \
     audio.a2dp.default \
     audio.primary.msm8937 \
