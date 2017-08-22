f01c1e0d09d15064112921afe63756ae85c1297e
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:21 +0200

    msm8937-common: Build Gatekeeper HIDL HAL interface
    Change-Id: I4140f7f17188900d23562e9dfc1e351e1988335f

--- msm8937.mk
+++ msm8937.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
     sap.conf \
     xtwifi.conf
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # Init scripts
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
