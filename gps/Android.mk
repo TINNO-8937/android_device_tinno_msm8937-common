e72dfc89ede03086747c46d9155d4034b96da7e0
Bruno Martins <bgcngm@gmail.com>
2018-01-07 23:57:38 +0000

    msm8937-common: Bump GPS HAL and location API to O
     * QC package: LA.UM.6.5.r1-05300-8x96.0
    
    Change-Id: I8246062a2c0d1e979a82be49a1723a69221859d6

--- gps/Android.mk
+++ gps/Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,6 +16,4 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter garlic wimlite hs2 hs3,$(TARGET_DEVICE)),)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
-endif
