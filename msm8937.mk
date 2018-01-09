8d6e0e368e34eabdd008b8e5f2c22b3969e4822c
Rashed Abdel-Tawab <rashed@linux.com>
2018-01-08 19:21:59 -0800

    msm8937-common: Build RCS and telephony-ext
    Change-Id: I456aa2db554647f4f6303964f7fb071f98b923ac

--- msm8937.mk
+++ msm8937.mk
@@ -284,6 +284,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
 
+# RCS
+PRODUCT_PACKAGES += \
+    rcs_service_aidl \
+    rcs_service_aidl.xml \
+    rcs_service_api \
+    rcs_service_api.xml
+
 # Qualcomm dependencies
 PRODUCT_PACKAGES += \
     libtinyxml
@@ -306,6 +313,13 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl
+
+# Telephony
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # USB
 PRODUCT_PACKAGES += \
