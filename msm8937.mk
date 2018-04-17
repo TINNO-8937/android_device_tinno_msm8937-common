232d2238362186c635647d26ef4a362df92b2f45
Eric Meddaugh <eric@meddaughs.com>
2018-04-17 10:16:24 -0400

    [DNM]msm8937-common: Add loggy and enabled permissive
    * This is for testing/debug only.

--- msm8937.mk
+++ msm8937.mk
@@ -365,3 +365,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:$(TARGET_COPY_OUT_VENDOR)/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
+# loggy
+PRODUCT_PACKAGES += \
+    loggy.sh
