ceb6acb873de3d12c0e3485c33dedb8ae82e4385
Louis Popi <theh2o64@gmail.com>
2018-07-14 15:45:03 +0200

    msm8937-common: Import common properties

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -171,6 +171,9 @@ TARGET_PER_MGR_ENABLED := true
 # Power
 TARGET_POWERHAL_VARIANT := qcom
 
+# Properties
+TARGET_SYSTEM_PROP := $(COMMON_PATH)/system.prop
+
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
