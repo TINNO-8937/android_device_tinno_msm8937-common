918ee3b0f10f4778adbc6c828345065a7535411e
Louis Popi <theh2o64@gmail.com>
2018-08-05 11:11:29 +0200

    msm8937-common: Add initial goodix sepolicy
    * Kanged from kenzo and whyred

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -185,7 +185,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
 
 # Sensors
 USE_SENSOR_MULTI_HAL := true
