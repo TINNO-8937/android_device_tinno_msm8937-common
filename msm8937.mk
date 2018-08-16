f4a6d9fa1c7efc8157ff1ea2b029300f19765fe0
Bruno Martins <bgcngm@gmail.com>
2018-08-16 15:19:28 +0100

    msm8937-common: Build common basic USB HAL
    Change-Id: I0e0809a2338f006063c5160bb7aec36a555cebab

--- msm8937.mk
+++ msm8937.mk
@@ -338,6 +338,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.basic
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
