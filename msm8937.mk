32dd1928b8e0db801c2453623cad275a850c4164
Sumit Das <sumidas@codeaurora.org>
2018-11-26 15:01:19 -0800

    msm8937-common: FR53248:Android Q::Remove hidden API references in ims.apk
    Add telephony-qti-utils libarary to product packages
    
    Change-Id: Ie8651ebadc246c21bd5eb69ef060fe32d8f5f7c8

--- msm8937.mk
+++ msm8937.mk
@@ -340,6 +340,8 @@ PRODUCT_PACKAGES += \
     telephony-ext \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
 
 
 PRODUCT_BOOT_JARS += \
