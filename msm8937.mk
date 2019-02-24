533de27cde587d66b46025b4fabbaefba63f1ced
Louis Popi <theh2o64@gmail.com>
2019-02-24 07:48:33 +0100

    Revert "[DNM] msm8937-common: Nuke camera products"
    This reverts commit 3ae36fdfda1dbea9c11cd5cba62834dc3f62e423.

--- msm8937.mk
+++ msm8937.mk
@@ -119,14 +119,14 @@ PRODUCT_PACKAGES += \
     libbt-vendor
 
 # Camera
-#PRODUCT_PACKAGES += \
-#    android.hardware.camera.provider@2.4-impl \
-#    android.hardware.camera.provider@2.4-service \
-#    camera.device@3.2-impl \
-#    camera.msm8937 \
-#    Snap \
-#    vendor.qti.hardware.camera.device@1.0 \
-#    vendor.qti.hardware.camera.device@1.0_vendor
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@3.2-impl \
+    camera.msm8937 \
+    Snap \
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Display
 PRODUCT_PACKAGES += \
