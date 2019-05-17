1e58f0f4e0338560c699f3e5f2dc4077725e1f98
The-DarkBeast <pranay@firehound.org>
2019-05-17 22:52:53 +0200

    msm8937-common: Update GPS stack to pie
    GPS HAL: LA.UM.7.6.r1-04300-89xx.0
    GNSSPPS: LA.UM.7.6.r1-04300-89xx.0
    GPS configs: daisy(PKQ1.180917.001-V10.0.1.0.PDLMIFJ)
    GPS blob list: daisy(PKQ1.180917.001-V10.0.1.0.PDLMIFJ)

--- gps/core/Android.mk
+++ gps/core/Android.mk
@@ -1,3 +1,4 @@
+ifneq ($(BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE),)
 ifneq ($(BUILD_TINY_ANDROID),true)
 
 LOCAL_PATH := $(call my-dir)
@@ -5,11 +6,12 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libloc_core
-LOCAL_MODULE_PATH_32 := $(TARGET_OUT_VENDOR)/lib
-LOCAL_MODULE_PATH_64 := $(TARGET_OUT_VENDOR)/lib64
+LOCAL_VENDOR_MODULE := true
 LOCAL_MODULE_TAGS := optional
 
-ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
+ifeq ($(TARGET_DEVICE),apq8026_lw)
+LOCAL_CFLAGS += -DPDK_FEATURE_SET
+else ifeq ($(BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET),true)
 LOCAL_CFLAGS += -DPDK_FEATURE_SET
 endif
 
@@ -19,8 +21,7 @@ LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libgps.utils \
     libdl \
-    liblog \
-    libloc_pla
+    liblog
 
 LOCAL_SRC_FILES += \
     LocApiBase.cpp \
@@ -29,9 +30,6 @@ LOCAL_SRC_FILES += \
     LocDualContext.cpp \
     loc_core_log.cpp \
     data-items/DataItemsFactoryProxy.cpp \
-    data-items/common/ClientIndex.cpp \
-    data-items/common/DataItemIndex.cpp \
-    data-items/common/IndexFactory.cpp \
     SystemStatusOsObserver.cpp \
     SystemStatus.cpp
 
@@ -45,6 +43,7 @@ LOCAL_C_INCLUDES:= \
     $(LOCAL_PATH)/observer \
 
 LOCAL_HEADER_LIBRARIES := \
+    libutils_headers \
     libgps.utils_headers \
     libloc_pla_headers \
     liblocation_api_headers
@@ -63,3 +62,4 @@ LOCAL_EXPORT_C_INCLUDE_DIRS := \
 include $(BUILD_HEADER_LIBRARY)
 
 endif # not BUILD_TINY_ANDROID
+endif # BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE
