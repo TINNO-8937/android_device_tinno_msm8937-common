d3d939b80f51e6b37881f4b5980ddc80c611ec64
The-DarkBeast <pranay@firehound.org>
2019-07-24 08:10:44 +0200

    msm8937-common: Cleanup display properties
    * Taken from https://github.com/RiteshSaxena/android_device_xiaomi_land/blob/pie-caf/vendor_prop.mk#L85

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -71,19 +71,26 @@ persist.cne.feature=1
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.sf.hw=1 \
 debug.sf.enable_hwc_vds=1 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=1 \
-debug.sf.recomputecrop=0 \
-debug.enable.sglscale=1 \
+debug.egl.hw=0 \
+persist.hwc.mdpcomp.enable=true \
 debug.mdpcomp.logs=0 \
 dev.pm.dyn_samplingrate=1 \
-persist.hwc.enable_vds=1 \
-persist.hwc.mdpcomp.enable=true \
 persist.demo.hdmirotationlock=false \
+debug.enable.sglscale=1 \
+sdm.debug.disable_skip_validate=1 \
+debug.sf.recomputecrop=0 \
+vendor.display.enable_default_color_mode=1 \
+vendor.gralloc.enable_fb_ubwc=1 \
+vendor.display.disable_skip_validate=1 \
+ro.opengles.version=196610 \
+ro.qualcomm.cabl=0 \
+ro.vendor.display.cabl=2 \
 ro.sf.lcd_density=441 \
-sdm.debug.disable_skip_validate=1
+persist.debug.wfd.enable=1 \
+persist.hwc.enable_vds=1
 
 # FM radio
 PRODUCT_PROPERTY_OVERRIDES += \
