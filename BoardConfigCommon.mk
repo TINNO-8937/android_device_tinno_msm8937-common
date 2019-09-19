8dfb462016087bbab57ce2cac6ab9552883268d4
Nicholas Magill <nmagill1979@gmail.com>
2019-09-19 22:24:49 +0100

    msm8937-common: Temporary sepolicy fix
    Even though kernel is permissive, prevents this build error:
    mkuserimg_mke2fs.py ERROR: Failed to run e2fsdroid_cmd: set_selinux_xattr: No such file or directory searching for label "/firmware"

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -193,7 +193,7 @@ endif
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy_temp
 TARGET_SEPOLICY_DIR := msm8937
 
 ifneq ($(TARGET_BUILD_VARIANT),user)
